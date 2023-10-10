f5671223714dbcecdfc6b0dd7c44c2205a055c23
suaida <suaidasahad@gmail.com>
2023-10-10 20:08:49 +0530

    registration completed

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 from .forms import RegistrationalForm
+from .models import Account
 # Create your views here.
 
 def Home(request):
@@ -10,7 +11,21 @@ def Login(request):
     return render (request,'Authentication/login.html')
 
 def Register(request):
-    form = RegistrationalForm()
+    if request.method == 'POST':
+        form = RegistrationalForm(request.POST)
+        if form.is_valid():
+            first_name=form.cleaned_data['first_name']
+            last_name=form.cleaned_data['last_name']
+            phone_number=form.cleaned_data['phone_number']
+            email=form.cleaned_data['email']
+            password=form.cleaned_data['password']
+            username=email.split("@")[0]
+
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationalForm()
     context={
         'form':form,
 
@@ -18,4 +33,4 @@ def Register(request):
 
     
 
-    return render (request,'Authentication/register.html')
+    return render (request,'Authentication/register.html',context)
